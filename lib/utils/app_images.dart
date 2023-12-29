7c83d813e5241ffa14a4a5a8acf46f75b1252d57
sdtbdc <bsmrstunadimcse@gmail.com>
2023-12-29 19:07:48 +0600

    start_project

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,29 @@
+
+
+
+class  AppImages {
+
+
+
+static const String baseUrl = "assets/images" ;
+
+
+static const String abel = "$baseUrl/abel.png" ;
+static const String friends = "$baseUrl/friends.png" ;
+static const String future = "$baseUrl/future.png" ;
+static const String john = "$baseUrl/john.png" ;
+static const String knowledge = "$baseUrl/knowledge.png" ;
+static const String masterCard = "$baseUrl/master_card.png" ;
+static const String party = "$baseUrl/party.png" ;
+static const String paypal = "$baseUrl/paypal.png" ;
+static const String profile = "$baseUrl/profile.png" ;
+static const String visa = "$baseUrl/visa.png" ;
+
+
+
+
+
+
+
+
+}
