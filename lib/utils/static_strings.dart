3d6588d33e1ab06c4b0850a3879d2e3408c6a7dc
rafsanopi <rafsan3@dti.ac>
2024-01-03 10:11:22 +0600

    static string

---
+++ lib/utils/static_strings.dart
@@ -0,0 +1,101 @@
+class AppStrings {
+  // Sign in
+  static const String unlockPremiumAccess =
+      "Unlock premium access for unlimited Dialoginess!";
+
+  static const String getPremium = "Get Premium";
+  static const String skipForNow = "Skip for now";
+  static const String welcomeToThe = "Welcome to the";
+  static const String dialogiApp = "Dialogi App";
+  static const String enterYourEmail = "Enter your email";
+  static const String enteryourpassword = "Enter your password";
+  static const String forgetPassword = "Forget Password";
+  static const String signIn = "Sign In";
+  static const String or = "Or";
+  static const String continuewithGoogle = "Continue with Google";
+  static const String continuewithApple = "Continue with Apple";
+  static const String donthaveanaccount = "Don’t have an account?";
+
+  //Sign Up
+  static const String createnewaccount = "Create new account";
+  static const String joinusforbetterexperience =
+      "Join us for better experience";
+  static const String fullname = "Full name";
+  static const String email = "Email";
+  static const String dateofbirth = "Date of birth";
+  static const String address = "Address";
+  static const String continuee = "Continue";
+  static const String alreadyhaveanaccount = "Already have an account?";
+
+  static const String setPassword = "Set Password";
+  static const String passwordshouldbemore =
+      "Password should be more that 8 characters or more.";
+  static const String reenteryourpassword = "Re-enter your password";
+  static const String setProfilePicture = "Set Profile Picture";
+  static const String pleaseuploadApicture =
+      "Please upload a picture to complete your profile.";
+  static const String skip = "Skip";
+  static const String getStarted = "Get Started";
+  static const String photohasbeenuploaded = "Photo has been uploaded.";
+
+  //Forget Password
+  static const String pleaseEnteryourEmailAddresstoreset =
+      "Please enter your email address to reset your password.";
+
+  static const String getOTP = "Get OTP";
+  static const String wehavesentaverificationcode =
+      "We have sent a verification code to your email address. Please check and enter the code here.";
+  static const String didntreceivethecode = "Didn’t receive the code?";
+  static const String verify = "Verify";
+  static const String resetPassword = "Reset Password";
+
+  //Home
+  static const String settings = "Settings";
+  static const String logout = "Logout";
+  static const String upgradetoPremiumfor =
+      "Upgrade to Premium for better experience";
+  static const String gotoSubscriptions = "Go to Subscriptions";
+
+  //Chat
+  static const String chat = "Chat";
+  static const String chatList = "Chat List";
+  static const String typemessage = "Type message";
+
+  //Nav Bar
+  static const String home = "Home";
+  static const String category = "Category";
+  static const String friends = "Friends";
+  static const String profile = "Profile";
+
+  //Category
+  static const String subCategories = "Sub-Categories";
+  static const String closeFriends = "Close Friends";
+  static const String bestFriends = "Best Friends";
+  static const String penFriends = "Pen Friends";
+  static const String enteryouranswer = "Enter your answer";
+  static const String next = "Next";
+  static const String wanttojointhediscussion =
+      "Want to join the discussion with others?";
+  static const String upgradetoPremium = "Upgrade to Premium";
+  static const String greatJob = "Great Job!";
+  static const String youHaveCompleted = "You have completed all questions";
+  static const String gotocategories = "Go to categories";
+
+   //Settings
+  static const String changePassword = "Change Password";
+  static const String subscriptions = "Subscriptions";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String aboutUs = "About Us";
+  static const String support = "Support";
+  static const String fAQ = "FAQ";
+  static const String deleteAccount = "Delete Account";
+  static const String currentpassword = "Current password";
+  static const String newpassword = "New password";
+  static const String reEnterNewpassword = "Re-enter new password";
+  static const String update = "Update";
+  // static const String newpassword = "Please enter your email address to reset your password.";
+  // static const String newpassword = "New password";
+
+
+
+}
