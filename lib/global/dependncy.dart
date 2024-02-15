62d2a211340b4f445a2597c910b9b463ccb79252
masumraj1 <promotionbd321@gmail.com>
2024-02-15 10:58:21 +0600

    controller created

--- lib/global/dependncy.dart
+++ lib/global/dependncy.dart
@@ -1,4 +1,5 @@
 
+import 'package:dialogi_app/controllers/sign_in_controller/sign_in_controller.dart';
 import 'package:dialogi_app/view/screens/auth/sign_up/sign_up_controller/sign_up_controller.dart';
 import 'package:dialogi_app/view/screens/home/home_controller/home_controller.dart';
 import 'package:dialogi_app/view/screens/profile/profile/profile_controller/profile_controller.dart';
@@ -8,6 +9,7 @@ class DependancyInjection extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => SignUpController(), fenix: true);
+    Get.lazyPut(() => SignInController(), fenix: true);
     Get.lazyPut(() => Homecontroller(), fenix: true);
     Get.lazyPut(() => ProfileController(), fenix: true);
   }
--- lib/view/screens/auth/sign_in/sign_in/sign_in_screen.dart
+++ lib/view/screens/auth/sign_in/sign_in/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dialogi_app/controllers/sign_in_controller/sign_in_controller.dart';
 import 'package:dialogi_app/core/app_routes.dart';
 import 'package:dialogi_app/utils/app_colors.dart';
 import 'package:dialogi_app/utils/app_icons.dart';
@@ -23,204 +24,206 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 44,
-            ),
+      body: GetBuilder<SignInController>(builder: (controller) {
+        return SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 44,
+              ),
 
-            /// welcome text
-            CustomText(
-              textAlign: TextAlign.start,
-              text: AppStrings.welcomeToThe,
-              fontSize: 18.h,
-              fontWeight: FontWeight.w500,
-              bottom: 8,
-            ),
-            CustomText(
-              textAlign: TextAlign.start,
-              text: AppStrings.dialogiApp,
-              fontSize: 30.h,
-              fontWeight: FontWeight.w500,
-              color: AppColors.blue_500,
-            ),
-
-            SizedBox(
-              height: 64.h,
-            ),
-
-            ///email
-            CustomTextField(
-              keyboardType: TextInputType.text,
-              textAlign: TextAlign.start,
-              hintText: AppStrings.enterYourEmail,
-              hintStyle: GoogleFonts.prompt(
-                  fontSize: 14.h,
-                  fontWeight: FontWeight.w400,
-                  color: AppColors.black_300),
-              inputTextStyle: GoogleFonts.prompt(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16.h,
-                  color: AppColors.black_500),
-              fieldBorderColor: Colors.white,
-              fieldBorderRadius: 8,
-              isPrefixIcon: true,
-              prefixIcon: Icon(
-                Icons.mail_outline,
-                size: 24.h,
+              /// welcome text
+              CustomText(
+                textAlign: TextAlign.start,
+                text: AppStrings.welcomeToThe,
+                fontSize: 18.h,
+                fontWeight: FontWeight.w500,
+                bottom: 8,
+              ),
+              CustomText(
+                textAlign: TextAlign.start,
+                text: AppStrings.dialogiApp,
+                fontSize: 30.h,
+                fontWeight: FontWeight.w500,
                 color: AppColors.blue_500,
               ),
-            ),
 
-            const SizedBox(
-              height: 24,
-            ),
-
-            ///password
-            CustomTextField(
-              isPassword: true,
-              keyboardType: TextInputType.text,
-              textAlign: TextAlign.start,
-              hintText: AppStrings.enteryourpassword,
-              hintStyle: GoogleFonts.prompt(
-                  fontSize: 14.h,
-                  fontWeight: FontWeight.w400,
-                  color: AppColors.black_300),
-              inputTextStyle: GoogleFonts.prompt(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16.h,
-                  color: AppColors.black_500),
-              fieldBorderColor: Colors.white,
-              fieldBorderRadius: 8,
-              isPrefixIcon: true,
-              prefixIcon: Icon(
-                Icons.lock_outlined,
-                size: 24.h,
-                color: AppColors.blue_500,
+              SizedBox(
+                height: 64.h,
               ),
-            ),
 
-            ///forget password
-            GestureDetector(
-              onTap: () {
-                Get.toNamed(AppRoutes.forgetPasswordScreen);
-              },
-              child: CustomText(
-                text: AppStrings.forgotPassword,
-                color: AppColors.red_500,
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-                top: 24.h,
-                bottom: 24.h,
+              ///email
+              CustomTextField(
+                keyboardType: TextInputType.text,
+                textAlign: TextAlign.start,
+                hintText: AppStrings.enterYourEmail,
+                hintStyle: GoogleFonts.prompt(
+                    fontSize: 14.h,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.black_300),
+                inputTextStyle: GoogleFonts.prompt(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.h,
+                    color: AppColors.black_500),
+                fieldBorderColor: Colors.white,
+                fieldBorderRadius: 8,
+                isPrefixIcon: true,
+                prefixIcon: Icon(
+                  Icons.mail_outline,
+                  size: 24.h,
+                  color: AppColors.blue_500,
+                ),
               ),
-            ),
 
-            ///signIn button
-            CustomElevatedButton(
-              onPressed: () {
-                Get.toNamed(AppRoutes.homeScreen);
-              },
-              titleText: AppStrings.signIn,
-              buttonWidth: MediaQuery.of(context).size.width,
-            ),
-
-            ///or
-            Center(
-              child: CustomText(
-                text: 'Or',
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-                top: 24.h,
-                bottom: 24.h,
+              const SizedBox(
+                height: 24,
               ),
-            ),
 
-            ///google
-            Container(
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.symmetric(vertical: 14),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CustomImage(
-                    imageSrc: AppIcons.google,
-                    size: 24,
-                    imageType: ImageType.svg,
-                  ),
-                  CustomText(
-                    text: AppStrings.continuewithGoogle,
-                    color: AppColors.black_500,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                    left: 8.w,
-                  ),
-                ],
+              ///password
+              CustomTextField(
+                isPassword: true,
+                keyboardType: TextInputType.text,
+                textAlign: TextAlign.start,
+                hintText: AppStrings.enteryourpassword,
+                hintStyle: GoogleFonts.prompt(
+                    fontSize: 14.h,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.black_300),
+                inputTextStyle: GoogleFonts.prompt(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.h,
+                    color: AppColors.black_500),
+                fieldBorderColor: Colors.white,
+                fieldBorderRadius: 8,
+                isPrefixIcon: true,
+                prefixIcon: Icon(
+                  Icons.lock_outlined,
+                  size: 24.h,
+                  color: AppColors.blue_500,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
 
-            ///apple
-            Container(
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.symmetric(vertical: 14),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CustomImage(
-                    imageSrc: AppIcons.apple,
-                    size: 24,
-                    imageType: ImageType.svg,
-                  ),
-                  CustomText(
-                    text: AppStrings.continuewithApple,
-                    color: AppColors.black_500,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18,
-                    left: 8.w,
-                  ),
-                ],
+              ///forget password
+              GestureDetector(
+                onTap: () {
+                  Get.toNamed(AppRoutes.forgetPasswordScreen);
+                },
+                child: CustomText(
+                  text: AppStrings.forgotPassword,
+                  color: AppColors.red_500,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  top: 24.h,
+                  bottom: 24.h,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 34.h,
-            ),
 
-            ///don't have an account
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const CustomText(
-                    text: AppStrings.donthaveanaccount,
-                    fontSize: 16,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Get.toNamed(AppRoutes.signUpScreen);
-                    },
-                    child: const CustomText(
-                      text: AppStrings.signUp,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.blue_500,
-                      left: 4,
+              ///signIn button
+              CustomElevatedButton(
+                onPressed: () {
+                  Get.toNamed(AppRoutes.homeScreen);
+                },
+                titleText: AppStrings.signIn,
+                buttonWidth: MediaQuery.of(context).size.width,
+              ),
+
+              ///or
+              Center(
+                child: CustomText(
+                  text: 'Or',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  top: 24.h,
+                  bottom: 24.h,
+                ),
+              ),
+
+              ///google
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 14),
+                decoration: BoxDecoration(
+                    color: Colors.white, borderRadius: BorderRadius.circular(8)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomImage(
+                      imageSrc: AppIcons.google,
+                      size: 24,
+                      imageType: ImageType.svg,
                     ),
-                  ),
-                ],
+                    CustomText(
+                      text: AppStrings.continuewithGoogle,
+                      color: AppColors.black_500,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18,
+                      left: 8.w,
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+              const SizedBox(
+                height: 24,
+              ),
+
+              ///apple
+              Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.symmetric(vertical: 14),
+                decoration: BoxDecoration(
+                    color: Colors.white, borderRadius: BorderRadius.circular(8)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomImage(
+                      imageSrc: AppIcons.apple,
+                      size: 24,
+                      imageType: ImageType.svg,
+                    ),
+                    CustomText(
+                      text: AppStrings.continuewithApple,
+                      color: AppColors.black_500,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18,
+                      left: 8.w,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 34.h,
+              ),
+
+              ///don't have an account
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CustomText(
+                      text: AppStrings.donthaveanaccount,
+                      fontSize: 16,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Get.toNamed(AppRoutes.signUpScreen);
+                      },
+                      child: const CustomText(
+                        text: AppStrings.signUp,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.blue_500,
+                        left: 4,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },),
     );
   }
 }
